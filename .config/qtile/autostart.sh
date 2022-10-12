b3fbe1c0d57821dc3027310c6322c7dfc4f92dc6
itsUrcute <itsurcute@gmail.com>
2022-10-12 16:48:10 +0530

    bump

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# disable beep
+xset -b
+
+# Set keyboard (in x) to gb and switch capslock to ctrl
+setxkbmap -option ctrl:nocaps
+
+# Start compositor
+picom -b
+
+# hide cursor
+unclutter --jitter 10 --ignore-scrolling --start-hidden --fork
+
+# Screenshot utility
+flameshot &
+
+# other apps
+discord &
+firefox &
+alacritty &
+
+# Wallpaper
+wal -i Assets/Aesthetic2.png &
